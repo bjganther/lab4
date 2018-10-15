9ea3cb81f62e81599db887687c0714e278acca8e
Becina Ganther <becina_ganther@college.harvard.edu>
2018-10-15 13:30:54 -0400

    comments don't save yet

---
+++ views/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <ul>
+        <li v-for="(todo, index) in todos">
+            {{ todo }}
+        </li>
+    </ul>
+
+</template>
+
+<script>
+    export default {
+        props: {
+            todos: Array
+        }
+    }
+</script>
+
+<style>
+</style>
