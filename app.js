2e6980512ac08a324c256b2e3cc47f378a1687f8
Jung Hur <junghur@college.harvard.edu>
2018-10-15 23:04:42 -0400

    done with singles.vue

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const expressVue = require('express-vue');
 const path = require('path');
+const sqlite3 = require('sqlite3');
+var bodyParser = require('body-parser');
+
+
+
+
+
 require('cross-fetch/polyfill');
 
 const hostname = '127.0.0.1';
@@ -10,6 +17,20 @@ const API_KEY = "8d33e1b0-b77e-11e8-bf0e-e9322ccde4db";
 // Initialize Express
 const app = express();
 app.use(express.static('static'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.text())
+
+//open database for comments
+let db = new sqlite3.Database('lab4database.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the SQlite database.');
+});
+
 
 
 // Options for express-vue
@@ -44,18 +65,18 @@ app.use(expressVueMiddleware);
 
 // List galleries
 app.get('/', (req, res) => {
-  
+
   const url = `https://api.harvardartmuseums.org/gallery?size=100&apikey=${API_KEY}`;
   //calls API for all galleries
   fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        let galleries = data.records;
-        res.renderVue('index.vue', {galleries});
-      });
+    .then(response => response.json())
+    .then(data => {
+      let galleries = data.records;
+      res.renderVue('index.vue', {galleries});
+    });
 });
-  
-          
+
+
 
 
 // List objects
@@ -63,33 +84,75 @@ app.get('/gallery/:gallery_id', (req, res) => {
   const objURL = `https://api.harvardartmuseums.org/object?size=100&apikey=${API_KEY}&gallery=${req.params.gallery_id}`;
   //calls API for individual gallery
   fetch(objURL)
-      .then(response => response.json())
-      .then(data => {
-        let objects= data.records;
-          res.renderVue('object.vue', {objects});
-      });
+    .then(response => response.json())
+    .then(data => {
+      let objects= data.records;
+      res.renderVue('object.vue', {objects});
+    });
 });
 
 // Show object
 app.get('/object/:objectnumber', (req, res) => {
+  let todos = ["one", "two", "three"];
   const singleURL = `https://api.harvardartmuseums.org/object?size=100&apikey=${API_KEY}&objectnumber=${req.params.objectnumber}`;
   //calls API for individual object
   fetch(singleURL)
-      .then(response => response.json())
-      .then(data => {
-        let singles = data.records[0];
-        console.log("singles")
-        console.log(singles);
-          res.renderVue('singles.vue', {singles});
-      });
+    .then(response => response.json())
+    .then(data => {
+      let singles = data.records[0];
+
+
+      res.renderVue('singles.vue', {singles, todos});
+    });
 });
 
 // Comment on object
-app.get('/objects/:object_id/comment', (req, res) => {
-  // TODO
+let allcomments = [];
+
+
+
+app.post('/object/:objectnumber/comment', (req, res) => {
+
+  console.log(req.body.todo);
+  console.log("116");
+
+  if(allcomments[req.params.objectnumber]) {
+    allcomments[req.params.objectnumber].push(req.body.todo);
+  }
+  else {
+    allcomments[req.params.objectnumber] = [];
+    allcomments[req.params.objectnumber].push(req.body.todo);
+  }
+  console.log(allcomments);
+
+
+
+
+  /*db.run(`INSERT INTO CommentsTable (comment, username, objectnumber) VALUES(?,?,?)`,
+  [`${comment}`, `${username}`, `${req.params.objectnumber}`], function(err) {
+    if (err) {
+        return console.log(err.message);
+    }
+  }); */
+  db.run(`INSERT INTO CommentsTable (comment, objectnumber) VALUES(?,?)`,
+    [`${req.body.todo}`, `${req.params.objectnumber}`], function(err) {
+      if (err) {
+        return console.log(err.message);
+      }
+    });
+
+  res.redirect(`/object/${req.params.objectnumber}`);
 });
+
+
+/*  let length= db.get(`SELECT COUNT (comment) FROM CommentsTable`);
+                    for (i = 0; i < length; i++) {
+                        this.todos.push(`SELECT comment FROM CommentsTable WHERE id=i`);
+                    }*/
 
 // Listen on socket
 app.listen(port, hostname, () => {
   console.log(`Server running on http://${hostname}:${port}/`);
 });
+
+
--- views/singles.vue
+++ views/singles.vue
@@ -5,25 +5,55 @@
 </style>
 
 <template>
-  <div>
-      <h1 class="titleBanner"> {{ singles.title }} </h1>
-      <img v-bind:src="singles.primaryimageurl">
-      <p class="title"> {{ singles.description }} </p>
-      People: {{ singles.people[0].name }}
-      <a v-bind:href="singles.url"> More Information </a>
+    <div>
+        <h1>Single Listing</h1>
 
+        {{ singles.title }}
+        People: {{ singles.people[0].name }}
+        <a v-bind:href="singles.url"> More Information </a>
+        
 
-  </div>
+        <form v-on:submit.prevent="add">
+            <input placeholder="TODO" type="text" v-model="todo">
+            <input type="submit" value="Add">
+        </form>
+
+        <ul>
+            <li v-for="todo in todos">
+                {{ todo }}
+
+            </li>
+        </ul>
+
+    </div>
+
 </template>
 
 <script>
 
-export default {
+  export default {
+
     data() {
-      return {};
+      return {
+        todo: '',
+        todos: []
+      }
+    },
+    methods: {
+      add() {
+
+        this.todos.push(this.todo);
+        //this.todos=this.todo;
+        // let commentobject = {"comm": "whatsup"};
+
+        this.todo = '';
+        // return this.todos;
+
+      }
     }
-}
+  }
 </script>
 
 <style>
 </style>
+
